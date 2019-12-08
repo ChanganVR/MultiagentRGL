cd71f2e0aeb580a39871de3b7764767d13fdb747
chiutaiyin <58499177+chiutaiyin@users.noreply.github.com>
2019-12-07 18:52:11 -0600

    Update value_action_predictor.py

--- crowd_nav/policy/value_action_predictor.py
+++ crowd_nav/policy/value_action_predictor.py
@@ -17,6 +17,12 @@ class ValueActionPredictor(nn.Module):
         self.value_network = mlp(config.gcn.X_dim, config.rgl_ppo.value_network_dims)
         self.action_network = mlp(config.gcn.X_dim, config.rgl_ppo.value_network_dims[:-1] +
                                   [config.action_space.rotation_samples * config.action_space.speed_samples + 1])
+        
+    def convert_to_mean_and_cov(self, action_feats): # action_feats: (batch, 5) 5->(mu_x, mu_y, s_x^2, s_y^2, s_xy)
+        mu = action_feats[:,:2] # (batch, 2)
+        cov = torch.cat((action_feats[:,2:3], action_feats[:,-1:], action_feats[:,-1:], action_feats[:,3:4]), dim=-1)\
+                   .view(-1,2,2) # (batch, 2, 2)
+        return mu, cov
 
     def forward(self, state):
         """ Embed state into a latent space. Take the first row of the feature matrix as state representation.
@@ -34,5 +40,6 @@ class ValueActionPredictor(nn.Module):
             state_embedding_act = self.graph_model_act(state)[:, 0, :]
             value = self.value_network(state_embedding_val)
             action_feat = self.action_network(state_embedding_act)
+            mu, cov = self.convert_to_mean_and_cov(action_feat)
             
-        return value, action_feat
+        return value, mu, cov
